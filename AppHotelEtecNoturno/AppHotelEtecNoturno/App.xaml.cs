f73be68a7a5050b49a75ce88132c4b0b77e233a3
TIAGO SILVA <frost.tiago@gmail.com>
2021-11-10 10:22:47 -0300

    comit inicial

---
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppHotelEtecNoturno
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.ContratacaoHospedagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/Model/Hospedagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotelEtecNoturno.Model
+{
+    public class Hospedagem
+    {
+        public Suite QuartoEscolhido { get; set; }
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public int Estadia
+        {
+            get
+            {
+                return DataCheckOut.Subtract(DataCheckIn).Days;
+            }
+        }
+        public double ValorTotal
+        {
+            get => ( (QntAdultos * QuartoEscolhido.DiariaAdulto) +
+                     (QntCriancas * QuartoEscolhido.DiariaCrianca)
+                   ) * Estadia;
+        }
+
+    }
+}
