a2c6204c89cdebb3b23539e1e211216c30c9b0a3
TIAGO SILVA <frost.tiago@gmail.com>
2021-11-18 22:27:23 -0300

    Avanços na implementação das validações e model.

--- AppHotelEtecNoturno/AppHotelEtecNoturno/App.xaml.cs
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppHotelEtecNoturno.Model;
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,6 +8,28 @@ namespace AppHotelEtecNoturno
 {
     public partial class App : Application
     {
+        public List<Suite> lista_suites = new List<Suite>
+        {
+            new Suite()
+            {
+               Nome = "Super Luxo",
+               DiariaAdulto = 110.0,
+               DiariaCrianca = 55.0
+            },
+            new Suite()
+            {
+               Nome = "Executiva",
+               DiariaAdulto = 90.0,
+               DiariaCrianca = 45.0
+            },
+            new Suite()
+            {
+               Nome = "Crise",
+               DiariaAdulto = 45.0,
+               DiariaCrianca = 20.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
--- AppHotelEtecNoturno/AppHotelEtecNoturno/Model/Hospedagem.cs
+++ AppHotelEtecNoturno/AppHotelEtecNoturno/Model/Hospedagem.cs
@@ -6,8 +6,34 @@ namespace AppHotelEtecNoturno.Model
 {
     public class Hospedagem
     {
-        public Suite QuartoEscolhido { get; set; }
-        public int QntAdultos { get; set; }
+        int qnt_adultos;
+        Suite quarto_escolhido;
+
+        public Suite QuartoEscolhido 
+        { 
+            get => quarto_escolhido; 
+            set
+            {
+                if (value == null)
+                    throw new Exception("Por favor, selecione sua acomodação");
+
+                quarto_escolhido = value;
+            }
+        }
+        public int QntAdultos 
+        {
+            get => qnt_adultos; 
+            set
+            {
+                if (value == 0)
+                    throw new Exception("Por favor, selecione a quantidade de adultos");
+                
+                qnt_adultos = value;
+            }
+        }
+
+
+
         public int QntCriancas { get; set; }
         public DateTime DataCheckIn { get; set; }
         public DateTime DataCheckOut { get; set; }
